6d3f2f274ea26a445877d57e6cbdf2d806a257a5
kis szilvi <kissszilvi12@gmail.com>
2020-04-12 19:46:43 +0200

    Added mobile application

---
+++ BasicExample/app/src/main/java/volleybasicexample/abhiandroid/com/volleybasicexample/ChildrenActivity.java
@@ -0,0 +1,22 @@
+package volleybasicexample.abhiandroid.com.volleybasicexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class ChildrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children);
+
+        // Create Button Dynamically
+        /*LinearLayout linearLayout = findViewById(R.id.constraintLayout);*/
+
+    }
+}
